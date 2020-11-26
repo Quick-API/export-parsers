0ec99e6fc0c1ebcb8a9d04c24d957665f0d77699
Julien SEIXAS (MrZyr0) <32927775+MrZyr0@users.noreply.github.com>
2020-11-26 14:23:42 +0100

    Create an example model of insomnia export for IDE
    autocompletion need a objet reference so this will help the IDE for that

---
+++ src/models/insomnia-export-model.js
@@ -0,0 +1,46 @@
+export default {
+    "_type": "",
+    "__export_format": 0,
+    "__export_date": "",
+    "__export_source": "",
+    "resources": [
+        {
+            "_id": "",
+            "parentId": "",
+            "modified": 0,
+            "created": 0,
+            "url": "",
+            "name": "",
+            "description": "",
+            "method": "",
+            "body": {
+                "mimeType": "",
+                "text": ""
+            },
+            "parameters": [],
+            "headers": [
+                {
+                    "name": "",
+                    "value": "",
+                    "id": ""
+                }
+            ],
+            "authentication": {},
+            "metaSortKey": 0,
+            "isPrivate": false,
+            "settingStoreCookies": true,
+            "settingSendCookies": true,
+            "settingDisableRenderRequestBody": false,
+            "settingEncodeUrl": true,
+            "settingRebuildPath": true,
+            "settingFollowRedirects": "",
+            "_type": "",
+            "data": {
+                "endpoint": "",
+            },
+            "color": null,
+            "environment": {},
+            "environmentPropertyOrder": null,
+        },
+    ]
+}
